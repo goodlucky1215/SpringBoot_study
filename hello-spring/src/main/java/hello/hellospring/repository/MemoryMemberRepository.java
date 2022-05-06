25f75bb58f65305e1e181a3774c3c20379383281
goodlucky1215 <goodlucky1215@naver.com>
2022-05-06 21:43:06 +0900

    회원 repostiory생성

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // Optional.ofNullable로 감싸면 null값인 경우, 클라이언트에서 처리를 할 수 있게 된다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // store에있는 값을 차례로 가져와서 name과 같으면 바로 반환을 시켜버리고,
+        // 끝까지찾다가 없으면 Optional.ofNullable이 적용 된다.
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
