2194cbdc16b1c2c8b6165d6424dd58e3cd8a26ab
goodlucky1215 <goodlucky1215@naver.com>
2022-06-06 22:39:49 +0900

    item관련 repo추가

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -15,11 +17,15 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     public Book findOne(Long itemId) {
-
+        return itemRepository.findOne(itemId);
     }
 
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
     }
 }
