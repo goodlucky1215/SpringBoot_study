6aaed88481951c6f96a6dd245fdf5604bd8b129e
goodlucky1215 <goodlucky1215@naver.com>
2022-06-13 20:16:54 +0900

    JPA entity설정

--- JPA활용/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA활용/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name="orders")
 @Getter @Setter
@@ -17,14 +19,14 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id") //fk가 이게 됨.
     private Member member;
 
-    @OneToMany(mappedBy = "order") //order라는 것에 의해서 매핍이 된다는 것임.
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) //order라는 것에 의해서 매핍이 된다는 것임.
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id") //1대1의 경우 연관관계의 주인은 액세스(조회, 삭제, 수정 등)이 더 많은 곳에 두는게 좋다.
     private Delivery delivery;
 
@@ -34,4 +36,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //== 연관관계 편의 매서드 ==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrder().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
