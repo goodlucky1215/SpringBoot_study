9060e279e56fd1358972232bec181d852c93d254
goodlucky1215 <goodlucky1215@naver.com>
2022-06-28 22:31:01 +0900

    핸들러 우선 순위

--- 스프링MVC1/servlet/src/main/java/hello/servlet/ServletInitializer.java
+++ 스프링MVC1/servlet/src/main/java/hello/servlet/ServletInitializer.java
@@ -3,6 +3,7 @@ package hello.servlet;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
