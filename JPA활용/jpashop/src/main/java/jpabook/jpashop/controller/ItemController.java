2d5b3a24b581214a78c9bf41a1f86342342594d3
goodlucky1215 <goodlucky1215@naver.com>
2022-07-11 20:16:52 +0900

    JPA

--- JPA활용/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ JPA활용/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,13 +11,42 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
 
-    @GetMapping("item/{itemId}/edit")
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String createForm(BookForm form){
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    @GetMapping("items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
     public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
         Book item = itemService.findOne(itemId);
 
@@ -29,10 +59,10 @@ public class ItemController {
         form.setIsbn(item.getIsbn());
 
         model.addAttribute("form",form);
-        return "item/updateItemForm";
+        return "items/updateItemForm";
     }
 
-    @PostMapping("item/{itemId}/edit")
+    @PostMapping("items/{itemId}/edit")
     public String updateItem(@ModelAttribute("form") BookForm form,
                              @PathVariable String itemId){
         Book book = new Book();
