ff6bdc7e68c39e85822af76c836dfa8618dd78f9
goodlucky1215 <goodlucky1215@naver.com>
2022-07-18 20:17:15 +0900

    db2

--- Spring DB 2편/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring DB 2편/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,26 +1,30 @@
 package hello.itemservice;
 
 import hello.itemservice.config.*;
+import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 
 
 @Import(MemoryConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+@SpringBootApplication//(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	/*
 	@Bean
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
-	}
+	}*/
 
 }
--- Spring DB 2편/itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
+++ Spring DB 2편/itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
@@ -6,8 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
+//@Component
 @RequiredArgsConstructor
 public class TestDataInit {
 
@@ -23,4 +27,12 @@ public class TestDataInit {
         itemRepository.save(new Item("itemB", 20000, 20));
     }
 
+    /*
+    @PostConstruct
+    public void initData1() {
+        log.info("test data init");
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+    */
 }
