9a51e745fdb21969e5f4b7af9617efb9c2ba2238
goodlucky1215 <goodlucky1215@naver.com>
2022-08-31 12:25:58 +0900

    springdatajpa ing

--- SSGTechShop/shop/src/main/java/rookie/shop/repository/CartRepositoryDataJPA.java
+++ SSGTechShop/shop/src/main/java/rookie/shop/repository/CartRepositoryDataJPA.java
@@ -1,7 +1,20 @@
 package rookie.shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import rookie.shop.entity.CartEntity;
+import rookie.shop.entity.ItemEntity;
 import rookie.shop.entity.MemberEntity;
 
-public interface CartRepositoryDataJPA extends JpaRepository<MemberEntity, Long> {
+import java.util.List;
+
+public interface CartRepositoryDataJPA extends JpaRepository<CartEntity, Long> {
+
+    //장바구니 목록
+    List<CartEntity> findByMember_IdLike(MemberEntity memberEntity);
+
+    //장바구니 상품 삭제
+    void deleteByMember_IdAndItem_IdLike(CartEntity cartEntity);
+
+    //특정 장바구니 제품 아이템
+    CartEntity findByItem_idAAndMember_IdLike(ItemEntity itemEntity, MemberEntity memberEntity);
 }
