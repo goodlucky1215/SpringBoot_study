a991fbe8e1df634d246b6ce0282e8ba05059a4b5
goodlucky1215 <goodlucky1215@naver.com>
2022-11-08 23:29:14 +0900

    lamda 버전 적용

---
+++ refactoring/src/main/java/hello/Pattern/Ch6/command/Remocon/RemoteLoaderRamda.java
@@ -0,0 +1,31 @@
+package hello.Pattern.Ch6.command.Remocon;
+
+public class RemoteLoaderRamda {
+
+    public static void main(String[] args) {
+        RemoteControl remoteControl = new RemoteControl();
+
+        Light livingRoomLight = new Light();
+        Light kitchenLight = new Light();
+        Stereo stereo = new Stereo();
+
+        //람다 표현식으로 사용할 수 있지만,
+        //command 인터페이스에 추상메소드가 하나일 때만 가능하다는 점을 유의해야한다.
+        remoteControl.setCommand(0,() -> livingRoomLight.on(),
+                ()-> livingRoomLight.off());
+        remoteControl.setCommand(1,() -> kitchenLight.on(),
+                ()-> kitchenLight.off());
+        remoteControl.setCommand(2,() -> stereo.on(),
+                ()-> stereo.setVolume(5));
+
+
+
+        System.out.println(remoteControl);
+
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+        remoteControl.onButtonWasPushed(2);
+        remoteControl.onButtonWasPushed(1);
+        remoteControl.offButtonWasPushed(1);
+    }
+}
